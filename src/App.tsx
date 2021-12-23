6c96aac81d178ef683d700448c676f677a376fec
Adam Paul <adam.paul@live.com>
2021-12-23 14:51:36 -0330

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,133 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import { createStage, isColliding } from "./gameHelpers";
 
-function App() {
+// Custom hooks
+import { useInterval } from "./util/useInterval";
+import { usePlayer } from "./util/usePlayer";
+import { useStage } from "./util/useStage";
+import { useGameStatus } from "./util/useGameStatus";
+
+// Components
+import Stage from "./components/Stage/Stage";
+import Display from "./components/Display/Display";
+import StartButton from "./components/StartButton/StartButton";
+
+// Styles
+import { StyledTetrisWrapper, StyledTetris } from "./App.styles";
+
+const App: React.FC = () => {
+  const [dropTime, setDropTime] = useState<null | number>(null);
+  const [gameOver, setGameOver] = useState(true);
+
+  const gameArea = useRef<HTMLDivElement>(null);
+
+  const { player, updatePlayerPos, resetPlayer, playerRotate } = usePlayer();
+  const { stage, setStage, rowsCleared } = useStage(player, resetPlayer);
+  const { score, setScore, rows, setRows, level, setLevel } =
+    useGameStatus(rowsCleared);
+
+  const movePlayer = (dir: number) => {
+    if (!isColliding(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({ x: dir, y: 0, collided: false });
+    }
+  };
+
+  const keyUp = ({ keyCode }: { keyCode: number }): void => {
+    if (!gameOver) {
+      // Change the droptime speed when user releases down arrow
+      if (keyCode === 40) {
+        setDropTime(1000 / level + 200);
+      }
+    }
+  };
+
+  const handleStartGame = (): void => {
+    // Need to focus the window with the key events on start
+    if (gameArea.current) gameArea.current.focus();
+    // Reset everything
+    setStage(createStage());
+    setDropTime(1000);
+    resetPlayer();
+    setScore(0);
+    setLevel(1);
+    setRows(0);
+    setGameOver(false);
+  };
+
+  const move = ({
+    keyCode,
+    repeat,
+  }: {
+    keyCode: number;
+    repeat: boolean;
+  }): void => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        movePlayer(-1);
+      } else if (keyCode === 39) {
+        movePlayer(1);
+      } else if (keyCode === 40) {
+        // Just call once
+        if (repeat) return;
+        setDropTime(30);
+      } else if (keyCode === 38) {
+        playerRotate(stage);
+      }
+    }
+  };
+
+  const drop = (): void => {
+    // Increase level when player has cleared 10 rows
+    if (rows > level * 10) {
+      setLevel((prev) => prev + 1);
+      // Also increase speed
+      setDropTime(1000 / level + 200);
+    }
+
+    if (!isColliding(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({ x: 0, y: 1, collided: false });
+    } else {
+      // Game over!
+      if (player.pos.y < 1) {
+        console.log("Game over!");
+        setGameOver(true);
+        setDropTime(null);
+      }
+      updatePlayerPos({ x: 0, y: 0, collided: true });
+    }
+  };
+
+  useInterval(() => {
+    drop();
+  }, dropTime);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledTetrisWrapper
+      role="button"
+      tabIndex={0}
+      onKeyDown={move}
+      onKeyUp={keyUp}
+      ref={gameArea}
+    >
+      <StyledTetris>
+        <div className="display">
+          {gameOver ? (
+            <>
+              <Display gameOver={gameOver} text="Game Over!" />
+              <StartButton callback={handleStartGame} />
+            </>
+          ) : (
+            <>
+              <Display text={`Score: ${score}`} />
+              <Display text={`Rows: ${rows}`} />
+              <Display text={`Level: ${level}`} />
+            </>
+          )}
+        </div>
+        <Stage stage={stage} />
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
-}
+};
 
 export default App;
